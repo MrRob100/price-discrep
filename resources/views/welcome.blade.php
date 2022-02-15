893838278109fb7e61f04449431c20ade09e3253
horidhenry <robarnold100@hotmail.com>
2022-02-15 14:58:00 +0000

    basic project with livewire installed

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Price Discrepencies</title>
+        @livewireStyles
+    </head>
+    <body>
+        Hi World
+        @livewireScripts
+    </body>
+</html>
